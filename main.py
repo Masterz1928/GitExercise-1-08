d934dd34d9819a211701b67fcfd17cf59c24338d
MingSong <tohrucooldoragon@gmail.com>
2025-04-13 22:58:45 +0800

    clock

--- main.py
+++ main.py
@@ -3,3 +3,22 @@ import time
 #git add .
 #git commit -m "name"
 #git push
+
+root = tk.Tk()
+
+root.title("Timer")
+root.geometry("800x800")
+
+def clock():
+    hour = time.strftime("%H")
+    minute = time.strftime("%M")
+    second = time.strftime("%S")
+
+    label_time.config(text=hour + ":" + minute + ":" + second)
+    label_time.after(1000, clock)
+
+label_time = tk.Label(root, text="time", font=("Arial", 50))
+label_time.pack()
+
+clock()
+root.mainloop()
