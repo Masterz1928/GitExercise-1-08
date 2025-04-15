27e402cd1dca61253990f2f28f1cc9c81179b480
MingSong <tohrucooldoragon@gmail.com>
2025-04-15 22:06:06 +0800

    added timer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import time 
+from tkinter import StringVar, messagebox
 #git add .
 #git commit -m "name"
 #git push
@@ -9,6 +10,7 @@ root = tk.Tk()
 root.title("Timer")
 root.geometry("800x800")
 
+#display clock
 def clock():
     hour = time.strftime("%H")
     minute = time.strftime("%M")
@@ -19,6 +21,50 @@ def clock():
 
 label_time = tk.Label(root, text="time", font=("Arial", 50))
 label_time.pack()
+
+#testing timer
+hours = StringVar()
+mins = StringVar()
+secs = StringVar()
+
+hours.set("00")
+mins.set("00")
+secs.set("00")
+
+hoursEntry = tk.Entry(root, width=3, font=("arial, 18"), textvariable=hours)
+hoursEntry.place(x=20, y=80)
+
+minsEntry = tk.Entry(root, width=3, font=("arial, 18"), textvariable=mins)
+minsEntry.place(x=70, y=80)
+
+secsEntry = tk.Entry(root, width=3, font=("arial, 18"), textvariable=secs)
+secsEntry.place(x=120, y=80)
+
+#timer button
+def set():
+    try: 
+        totaltime = int(hours.get())*3600 + int(mins.get())*60 + int(secs.get())
+    except ValueError:
+        messagebox.showerror("Invalid Input", "Please enter a valid number.")
+        return
+
+    countdown(totaltime)
+
+def countdown(timeleft):
+    if timeleft >= 0:
+        hourss,remainder = divmod(timeleft,3600)
+        minss,secss = divmod(remainder,60)
+        
+        hours.set("{0:2d}".format(hourss))
+        mins.set("{0:2d}".format(minss))
+        secs.set("{0:2d}".format(secss))
+
+        root.after(1000, countdown, timeleft-1)
+    else: 
+            messagebox.showinfo("TIMES UP")
+
+timerbtn = tk.Button(root, text="set timer", command= set)
+timerbtn.pack()
 
 clock()
 root.mainloop()
