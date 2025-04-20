b82c1c2a3299e847df1577163a4a81d03611760f
Harsimran <hsj030609@gmail.com>
2025-04-20 16:28:03 +0800

    Added in the Save function into the program and added in a initial directory when saving or opening the file

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import tkinter as tk # Getting tkinter into the program
 from tkinter import filedialog
+from tkinter import messagebox
 
 root = tk.Tk()
 root.title("Note Editor")
 root.geometry("1200x600")
 #root.state("zoomed") *Not sure to make it zoomed or not yet, but writing it here for referance*
+
+#Set variable for the file name to False, when first starting the program
+global open_status_name 
+open_status_name = False
 
 #Creating Functions Here
 # Creating New File 
@@ -15,14 +20,20 @@ def New_File():
     root.title("New Note")
     #Adding status bar for display
     Status_bar.config(text="New File    ")
+    global open_status_name 
+    open_status_name = False
 
 # Creating a opening function
 def Opening():
     #Clearing text box
     Text_Box.delete("1.0", tk.END)
     #Grab The file name
-    text_file = filedialog.askopenfilename(initialdir="C:/Users/", title="Open a File", filetypes=(("Text Files", "*.txt"),("All Files", "*.*")))
+    text_file = filedialog.askopenfilename(initialdir="C:/Users/Harsimran/Desktop/Documents/Notes", title="Open a File", filetypes=(("Text Files", "*.txt"),("All Files", "*.*")))
     
+    #Check if there is a file name and if yes, make it global
+    if text_file:
+        global open_status_name 
+        open_status_name = text_file
     #Updating Status bar 
     name = text_file
     Status_bar.config(text=f"{name}    ")
@@ -38,7 +49,7 @@ def Opening():
     text_file.close()
 # Creating a function to save a file as (in a .txt format) 
 def Saving_File_As():
-    text_file = filedialog.asksaveasfilename(defaultextension=".*", initialdir="C:/Users/", title="Save File As", filetypes=(("Text Files", "*.txt"),("All Files", "*.*")))
+    text_file = filedialog.asksaveasfilename(defaultextension=".*", initialdir="C:/Users/Harsimran/Desktop/Documents/Notes", title="Save File As", filetypes=(("Text Files", "*.txt"),("All Files", "*.*")))
     if text_file:
         #Update the status bar
         name = text_file
@@ -51,6 +62,22 @@ def Saving_File_As():
         text_file.write(Text_Box.get(1.0, tk.END))
         #close the file 
         text_file.close()
+
+# Creating a function to save file (to update the contents of the file tbh)
+def Saving_File():
+    global open_status_name
+    if open_status_name:
+        # Save the file 
+        text_file = open(open_status_name, "w")
+        text_file.write(Text_Box.get(1.0, tk.END))
+        #close the file 
+        text_file.close()
+        # Prompting user with a message box
+        Message_Pop = messagebox.showinfo(title="Saving File", message="Your file has been saved!")
+        Message_Pop.pack()
+        Status_bar.config(text=f"Saved: {open_status_name}    ")
+    else:
+        Saving_File_As()
 
 
 # Create main frame
@@ -77,7 +104,7 @@ TopMenuBar.add_cascade(label="File", menu=file_menu)
 file_menu.add_command(label="New", command=New_File)
 file_menu.add_command(label="Open", command=Opening)
 file_menu.add_command(label="Save as", command=Saving_File_As)
-file_menu.add_command(label="Save")
+file_menu.add_command(label="Save", command=Saving_File)
 
 #Adding editing menu 
 edit_menu = tk.Menu(TopMenuBar, tearoff=False)
